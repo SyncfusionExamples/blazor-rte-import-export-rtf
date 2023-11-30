8ccd349708897a4ea3e8456f9e9c8d4da9e0c070
Sathiskumar KR <sathiskumar.rajendran@syncfusion.com>
2023-11-30 18:16:59 +0530

    Sample(RTE_RTF): Create the sample for Import and Export to RTF in Blazor RTE

---
+++ RTE_Import-Export RTF/Data/SampleDataController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.Text.RegularExpressions;
+
+namespace RTE_Import_Export_RTF.Data
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SampleDataController : ControllerBase
+	{
+		private IWebHostEnvironment hostingEnv;
+
+		public SampleDataController(IWebHostEnvironment env)
+		{
+			this.hostingEnv = env;
+		}
+
+
+		[HttpPost]
+		[Route("Import")]
+		public void Import(IList<IFormFile> UploadFiles)
+		{
+			if (UploadFiles != null)
+			{
+				var file = UploadFiles[0];
+
+				string HtmlString = string.Empty;
+
+				using (var mStream = new MemoryStream())
+				{
+					new WordDocument(file.OpenReadStream(), FormatType.Rtf).Save(mStream, FormatType.Html);
+					mStream.Position = 0;
+					HtmlString = new StreamReader(mStream).ReadToEnd();
+				}
+				
+				string extract = ExtractBodyContent(HtmlString);
+				var str = extract.Replace("\r\n", "");
+				Response.Headers.Add("rteValue", str);
+			}
+		}
+
+		public string ExtractBodyContent(string html)
+		{
+			if (html.Contains("<html") && html.Contains("<body"))
+			{
+				return html.Remove(0, html.IndexOf("<body>") + 6).Replace("</body></html>", "");
+			}
+			return html;
+		}
+	}
+}
---
+++ RTE_Import-Export RTF/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using RTE_Import_Export_RTF.Data;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSyncfusionBlazor();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIf0x0QHxbf1xzZFBMYl9bQXFPMyBoS35RdURiW31edHFURGJeWUd3");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
